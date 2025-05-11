7b78dd9d7445c55dd406c4edfe81926295572f06
Andrei Carvalho <andreicarvs@gmail.com>
2025-05-11 10:54:56 -0300

    Feat: Criação do config_email.php com dados SMTP

---
+++ config_email.php
@@ -0,0 +1,25 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();  
+    $mail->Host = $_ENV['MAIL_HOST'];
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['MAIL_USERNAME']; 
+    $mail->Password = $_ENV['MAIL_PASSWORD']; 
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+} catch (Exception $e) {
+    echo "Erro na configuração do e-mail: {$mail->ErrorInfo}"; 
+}
+
