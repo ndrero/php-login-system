fb8d4fc64a46c8e1a41dbe38cdfbb1a5280eea34
Andrei Carvalho <andreicarvs@gmail.com>
2025-05-16 11:58:50 -0300

    Feat: Adiciona validação de token para alterar senha

--- atualizar_senha.php
+++ atualizar_senha.php
@@ -1,3 +1,9 @@
+<?php 
+
+session_start();
+include_once 'verificar_token.php';
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
---
+++ verificar_token.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once 'db.php';
+
+$tokenFromURL = $_GET['token'];
+
+$stmt = $pdo->prepare('SELECT id, expires_at FROM tokens WHERE token = :token');
+$stmt->execute([':token' => $tokenFromURL]);
+$token = $stmt->fetch();
+
+if(!$token) {
+    $_SESSION['warning'] = 'Token inválido';
+    header('Location: esqueci_senha.php');
+    exit();
+}
+
+if(strtotime($token['expires_at']) < time()) {
+    $_SESSION['warning'] = 'Token expirado';
+    header('Location: esqueci_senha.php');
+    exit();
+}
